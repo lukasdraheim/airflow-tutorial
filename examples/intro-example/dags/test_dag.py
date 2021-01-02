33488f2431c0c45a1d41eb484c25fd09786500ad
lukasdraheim <76648396+lukasdraheim@users.noreply.github.com>
2021-01-02 16:28:13 +0100

    Create test_dag.py

---
+++ examples/intro-example/dags/test_dag.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'adhoc':False,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'trigger_rule': u'all_success'
+}
+
+dag = DAG(dag_id='first_test', default_args=default_args. schedule_interval=None)
+
+def run_this_func(**context):
+    print('hi')
+
+with dag:
+    run_this_task = PythonOperator(
+        task_id='run this',
+        python_callable=run_this_func,
+        provide_context=True
+    )
